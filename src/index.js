f9d7d249ede6b09cf84bb0bdc7d026f138bb1106
david-shakaya <devidoz666@gmail.com>
2021-10-02 14:38:25 +0300

    added some file

--- src/index.js
+++ src/index.js
@@ -1,66 +1,69 @@
 import './sass/main.scss';
 import refs from './js/refs';
 
-const price = 5;
-let gamesАvailable = 0;
-let gamesQuantity = 0;
-let sum = 0;
+const state = {
+  gamesАvailable: 0,
+  gamesQuantity: 0,
+  sum: 0,
+};
 
-// refs.gamesAvailable.textContent = gamesАvailable;
+const price = 5;
+// let gamesАvailable = 0;
+// let gamesQuantity = 0;
+// let sum = 0;
 
 refs.btnPlus.addEventListener('click', plusToQuantity);
 refs.btnMinus.addEventListener('click', minusFromQuantity);
 refs.btnBuy.addEventListener('click', buyedGames);
 
 function plusToQuantity() {
-  gamesQuantity += 1;
-  sum += 5;
+  state.gamesQuantity += 1;
+  state.sum += 5;
   renderQuantity();
 
-  refs.btnMinus.classList.remove('player-scoreboard__btn-minus--disabled')
+  refs.btnMinus.classList.remove('player-scoreboard__btn-minus--disabled');
 }
 
 function minusFromQuantity() {
-  if (gamesQuantity > 0) {
-    gamesQuantity -= 1;
-    sum -= 5;
+  if (state.gamesQuantity > 0) {
+    state.gamesQuantity -= 1;
+    state.sum -= 5;
     renderQuantity();
   }
-  
-  if(gamesQuantity === 0){
-    refs.btnMinus.classList.add('player-scoreboard__btn-minus--disabled')
+
+  if (state.gamesQuantity === 0) {
+    refs.btnMinus.classList.add('player-scoreboard__btn-minus--disabled');
   }
 }
 
 function renderQuantity() {
-  refs.gamesQuantity.textContent = gamesQuantity;
-  refs.sum.innerHTML = `${sum} <span class="player-scoreboard__sum-icon">$</span>`;
+  refs.gamesQuantity.textContent = state.gamesQuantity;
+  refs.sum.innerHTML = `${state.sum} <span class="player-scoreboard__sum-icon">$</span>`;
 
   // .player-scoreboard__btn
 }
 
 function renderGamesAvailable() {
-  gamesАvailable += gamesQuantity;
-  refs.gamesAvailable.textContent = gamesАvailable;
+  state.gamesАvailable += state.gamesQuantity;
+  refs.gamesAvailable.textContent = state.gamesАvailable;
 }
 
 function buyedGames() {
-  if (gamesQuantity === 0) {
+  if (state.gamesQuantity === 0) {
     return;
   }
   renderGamesAvailable();
   resetNumbers();
-  refs.btnMinus.classList.add('player-scoreboard__btn-minus--disabled')
+  refs.btnMinus.classList.add('player-scoreboard__btn-minus--disabled');
   renderQuantity();
-  
 }
 
 function resetNumbers() {
-  gamesQuantity = 0;
-  sum = 0;
+  state.gamesQuantity = 0;
+  state.sum = 0;
 }
 
-
+export default state;
 
 // import '../node_modules/basiclightbox/dist/basiclightbox.min.css';
 
