04e878db6521716a4e29a21a564fd15759f8ef93
david-shakaya <devidoz666@gmail.com>
2021-10-03 20:59:17 +0300

    adds final edits

--- src/js/showsTextPrompts.js
+++ src/js/showsTextPrompts.js
@@ -1,26 +1,26 @@
-import {state} from './buysGames'
-import refs from './refs'
+import { state } from './buysGames';
+import refs from './refs';
 
 const textPromts = {
-    game: `Поздравляем с покупкой игр(ы).Нажми кноку играть и выбери сундук!`,
-    сhoice: 'Все верно, теперь выбери сундук',
-    win: 'Юхууу, вы выиграли!!!',
-    lose: 'К сожалению фортуна не на вашей стороне',
-    сlickStart() {
-        if(!state.gamesАvailable){
-           const noGames = 'Нужно купить игры для продолжения'
-           return `У вас осталось ${state.gamesАvailable} игр(ы).${noGames}`
-        }
-      return `У вас осталось ${state.gamesАvailable} игр(ы). Нажмите кнопку ИГРАТЬ!`
-    },
-}
+  game: `Поздравляем с покупкой игр(ы).Нажми кноку играть и выбери сундук!`,
+  сhoice: 'Все верно, теперь выбери сундук',
+  win: 'Юхууу, вы выиграли!!!',
+  lose: 'К сожалению фортуна не на вашей стороне',
+  сlickStart() {
+    if (!state.gamesАvailable) {
+      const noGames = 'Нужно купить игры для продолжения';
+      return `У вас осталось ${state.gamesАvailable} игр(ы).${noGames}`;
+    }
+    return `У вас осталось ${state.gamesАvailable} игр(ы). Нажмите кнопку ИГРАТЬ!`;
+  },
+};
 
 function showsTextPrompts(textVariable) {
-    renderTextPrompts(textVariable)
-}  
-
-function renderTextPrompts (text) {
-    refs.wrapperText.textContent = text
+  renderTextPrompts(textVariable);
 }
 
-export {showsTextPrompts, textPromts};
+function renderTextPrompts(text) {
+  refs.wrapperText.textContent = text;
+}
+
+export { showsTextPrompts, textPromts };
